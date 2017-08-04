612e7efe73dabbce67d5b476b40c9f6861ce74dc
robmachado <linux.rlm@gmail.com>
2017-08-04 08:55:49 -0300

    [UPD] stop ParamStandadize temporariamente

--- examples/Fake/FaketestEvtInfoEmpregador.php
+++ examples/Fake/FaketestEvtInfoEmpregador.php
@@ -1,6 +1,6 @@
 <?php
-//error_reporting(E_ALL);
-error_reporting(E_ERROR);
+error_reporting(E_ALL);
+//error_reporting(E_ERROR);
 ini_set('display_errors', 'On');
 require_once '../../bootstrap.php';
 
