f4603657ae466db03b4016800db7812a82818f32
robmachado <linux.rlm@gmail.com>
2017-08-11 16:30:26 -0300

    [UPD] ISSUE #76 Evento EvtTabRubrica

--- src/Factories/EvtTabRubrica.php
+++ src/Factories/EvtTabRubrica.php
@@ -61,18 +61,9 @@ class EvtTabRubrica extends Factory implements FactoryInterface
      */
     protected function toNode()
     {
-        $evtid = FactoryId::build(
-            $this->tpInsc,
-            $this->nrInsc,
-            $this->date,
-            $this->sequencial
-        );
-        $eSocial = $this->dom->getElementsByTagName("eSocial")->item(0);
-        $evtTabRubrica = $this->dom->createElement("evtTabRubrica");
-        $att = $this->dom->createAttribute('Id');
-        $att->value = $evtid;
-        $evtTabRubrica->appendChild($att);
-        
+        $ideEmpregador = $this->node->getElementsByTagName('ideEmpregador')->item(0);
+        //o idEvento pode variar de evento para evento
+        //então cada factory individualmente terá de construir o seu
         $ideEvento = $this->dom->createElement("ideEvento");
         $this->dom->addChild(
             $ideEvento,
@@ -92,26 +83,232 @@ class EvtTabRubrica extends Factory implements FactoryInterface
             $this->verProc,
             true
         );
-        $evtTabRubrica->appendChild($ideEvento);
-    
-        $ideEmpregador = $this->dom->createElement("ideEmpregador");
-        $this->dom->addChild(
-            $ideEmpregador,
-            "tpInsc",
-            $this->tpInsc,
-            true
-        );
-        $this->dom->addChild(
-            $ideEmpregador,
-            "nrInsc",
-            $this->nrInsc,
-            true
-        );
-        $evtTabRubrica->appendChild($ideEmpregador);
+        $this->node->insertBefore($ideEvento, $ideEmpregador);
         
-
-
-        $eSocial->appendChild($evtTabRubrica);
-        $this->sign($eSocial);
+        //tag deste evento em particular
+        $infoRubrica = $this->dom->createElement("infoRubrica");
+        //tag comum a todos os modos
+        $ideRubrica = $this->dom->createElement("ideRubrica");
+        $this->dom->addChild(
+            $ideRubrica,
+            "codRubr",
+            $this->std->codrubr,
+            true
+        );
+        $this->dom->addChild(
+            $ideRubrica,
+            "ideTabRubr",
+            $this->std->idetabrubr,
+            true
+        );
+        $this->dom->addChild(
+            $ideRubrica,
+            "iniValid",
+            $this->std->inivalid,
+            true
+        );
+        $this->dom->addChild(
+            $ideRubrica,
+            "fimValid",
+            !empty($this->std->fimvalid) ? $this->std->fimvalid : null,
+            false
+        );
+        //seleção do modo
+        if ($this->std->modo == 'INC') {
+            $node = $this->dom->createElement("inclusao");
+        } elseif ($this->std->modo == 'ALT') {
+            $node = $this->dom->createElement("alteracao");
+        } else {
+            $node = $this->dom->createElement("exclusao");
+        }
+        $node->appendChild($ideRubrica);
+        
+        if (!empty($this->std->dadosrubrica)) {
+            $dadosRubrica = $this->dom->createElement("dadosRubrica");
+            $this->dom->addChild(
+                $dadosRubrica,
+                "dscRubr",
+                $this->std->dadosrubrica->dscrubr,
+                true
+            );
+            $this->dom->addChild(
+                $dadosRubrica,
+                "natRubr",
+                $this->std->dadosrubrica->natrubr,
+                true
+            );
+            $this->dom->addChild(
+                $dadosRubrica,
+                "tpRubr",
+                $this->std->dadosrubrica->tprubr,
+                true
+            );
+            $this->dom->addChild(
+                $dadosRubrica,
+                "codIncCP",
+                $this->std->dadosrubrica->codinccp,
+                true
+            );
+            $this->dom->addChild(
+                $dadosRubrica,
+                "codIncIRRF",
+                $this->std->dadosrubrica->codincirrf,
+                true
+            );
+            $this->dom->addChild(
+                $dadosRubrica,
+                "codIncFGTS",
+                $this->std->dadosrubrica->codincfgts,
+                true
+            );
+            $this->dom->addChild(
+                $dadosRubrica,
+                "codIncSIND",
+                $this->std->dadosrubrica->codincsind,
+                true
+            );
+            $this->dom->addChild(
+                $dadosRubrica,
+                "repDSR",
+                $this->std->dadosrubrica->repdsr,
+                true
+            );
+            $this->dom->addChild(
+                $dadosRubrica,
+                "rep13",
+                $this->std->dadosrubrica->rep13,
+                true
+            );
+            $this->dom->addChild(
+                $dadosRubrica,
+                "repFerias",
+                $this->std->dadosrubrica->repferias,
+                true
+            );
+            $this->dom->addChild(
+                $dadosRubrica,
+                "repAviso",
+                $this->std->dadosrubrica->repaviso,
+                true
+            );
+            $this->dom->addChild(
+                $dadosRubrica,
+                "observacao",
+                !empty($this->std->dadosrubrica->observacao)
+                    ? $this->std->dadosrubrica->observacao
+                    : null,
+                false
+            );
+            
+            if (!empty($this->std->dadosrubrica->ideprocessocp)) {
+                foreach ($this->std->dadosrubrica->ideprocessocp as $cp) {
+                    $ideProcessoCP = $this->dom->createElement("ideProcessoCP");
+                    $this->dom->addChild(
+                        $ideProcessoCP,
+                        "tpProc",
+                        $cp->tpproc,
+                        true
+                    );
+                    $this->dom->addChild(
+                        $ideProcessoCP,
+                        "nrProc",
+                        $cp->nrproc,
+                        true
+                    );
+                    $this->dom->addChild(
+                        $ideProcessoCP,
+                        "extDecisao",
+                        $cp->extdecisao,
+                        true
+                    );
+                    $this->dom->addChild(
+                        $ideProcessoCP,
+                        "codSusp",
+                        $cp->codsusp,
+                        true
+                    );
+                    $dadosRubrica->appendChild($ideProcessoCP);
+                }
+            }
+            
+            if (!empty($this->std->dadosrubrica->ideprocessoirrf)) {
+                foreach ($this->std->dadosrubrica->ideprocessoirrf as $irrf) {
+                    $ideProcessoIRRF = $this->dom->createElement("ideProcessoIRRF");
+                    $this->dom->addChild(
+                        $ideProcessoIRRF,
+                        "nrProc",
+                        $irrf->nrproc,
+                        true
+                    );
+                    $this->dom->addChild(
+                        $ideProcessoIRRF,
+                        "codSusp",
+                        $irrf->codsusp,
+                        true
+                    );
+                    $dadosRubrica->appendChild($ideProcessoIRRF);
+                }
+            }
+            if (!empty($this->std->dadosrubrica->ideprocessofgts)) {
+                foreach ($this->std->dadosrubrica->ideprocessofgts as $fgts) {
+                    $ideProcessoFGTS = $this->dom->createElement("ideProcessoFGTS");
+                    $this->dom->addChild(
+                        $ideProcessoFGTS,
+                        "nrProc",
+                        $fgts->nrproc,
+                        true
+                    );
+                    $this->dom->addChild(
+                        $ideProcessoFGTS,
+                        "codSusp",
+                        $fgts->codsusp,
+                        true
+                    );
+                    $dadosRubrica->appendChild($ideProcessoFGTS);
+                }
+            }
+            if (!empty($this->std->dadosrubrica->ideprocessosind)) {
+                foreach ($this->std->dadosrubrica->ideprocessosind as $sind) {
+                    $ideProcessoSIND = $this->dom->createElement("ideProcessoSIND");
+                    $this->dom->addChild(
+                        $ideProcessoSIND,
+                        "nrProc",
+                        $sind->nrproc,
+                        true
+                    );
+                    $this->dom->addChild(
+                        $ideProcessoSIND,
+                        "codSusp",
+                        $sind->codsusp,
+                        true
+                    );
+                    $dadosRubrica->appendChild($ideProcessoSIND);
+                }
+            }
+            $node->appendChild($dadosRubrica);
+        }
+        if (!empty($this->std->novavalidade) && $this->std->modo == 'ALT') {
+            $newVal = $this->std->novavalidade;
+            $novaValidade = $this->dom->createElement("novaValidade");
+            $this->dom->addChild(
+                $ideRubrica,
+                "iniValid",
+                $newVal->inivalid,
+                true
+            );
+            $this->dom->addChild(
+                $ideRubrica,
+                "fimValid",
+                !empty($newVal->fimvalid) ? $newVal->fimvalid : null,
+                false
+            );
+            $node->appendChild($novaValidade);
+        }
+        
+        //finalização do xml
+        $infoRubrica->appendChild($node);
+        $this->node->appendChild($infoRubrica);
+        $this->eSocial->appendChild($this->node);
+        $this->sign();
     }
 }
