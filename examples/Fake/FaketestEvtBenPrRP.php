e25b1fa43ba64203db2ed83c2103198b2260bf28
Alex Assunção <alex.assuncao@convenia.com.br>
2017-08-23 11:09:54 -0300

    evtBenPrRP finalizado

--- examples/Fake/FaketestEvtBenPrRP.php
+++ examples/Fake/FaketestEvtBenPrRP.php
@@ -30,51 +30,25 @@ $configJson = json_encode($config, JSON_PRETTY_PRINT);
 $std              = new \stdClass();
 $std->sequencial  = 1;
 $std->indretif    = 1;
-$std->nrrecibo    = '123456';
+$std->nrrecibo    = '1111111111111111111111';
 $std->indapuracao = 1;
 $std->perapur     = '2017-08';
+$std->cpfbenef    = '11111111111';
 
-$ideestabadquir            = new \stdClass();
-$ideestabadquir->tpinscadq = 1;
-$ideestabadquir->nrinscadq = '11111111111111';
+$dmdev[0]            = new \stdClass();
+$dmdev[0]->tpbenef   = 1;
+$dmdev[0]->nrbenefic = '11111111111111111111';
+$dmdev[0]->idedmdev  = '11111111111111111111';
 
-$tpaquis[0]              = new \stdClass();
-$tpaquis[0]->indaquis    = 1;
-$tpaquis[0]->vlrtotaquis = 1500;
+$itens[0]             = new \stdClass();
+$itens[0]->codrubr    = '11111111111111111111';
+$itens[0]->idetabrubr = '11111111';
+$itens[0]->vrrubr     = 1500;
 
-$ideprodutor[0]              = new \stdClass();
-$ideprodutor[0]->tpinscprod  = 1;
-$ideprodutor[0]->nrinscprod  = 11111111111111;
-$ideprodutor[0]->vlrbruto    = 1500;
-$ideprodutor[0]->vrcpdescpr  = 0;
-$ideprodutor[0]->vrratdescpr = 0;
-$ideprodutor[0]->vrsenardesc = 0;
+$dmdev[0]->itens = $itens;
 
-$nfs[0]              = new \stdClass();
-$nfs[0]->serie       = '12345';
-$nfs[0]->nrdocto     = '11111111111111111111';
-$nfs[0]->dtemisnf    = '2017-08-22';
-$nfs[0]->vlrbruto    = 1500;
-$nfs[0]->vrcpdescpr  = 0;
-$nfs[0]->vrratdescpr = 0;
-$nfs[0]->vrsenardesc = 0;
+$std->dmdev = $dmdev;
 
-$ideprodutor[0]->nfs = $nfs;
-
-$infoprocjud[0]              = new \stdClass();
-$infoprocjud[0]->nrprocjud   = '111111111111111';
-$infoprocjud[0]->codsusp     = 11111111111111;
-$infoprocjud[0]->vrcpnret    = 1500;
-$infoprocjud[0]->vrratnret   = 1500;
-$infoprocjud[0]->vrsenarnret = 1500;
-
-$ideprodutor[0]->infoprocjud = $infoprocjud;
-
-$tpaquis[0]->ideprodutor = $ideprodutor;
-
-$ideestabadquir->tpaquis = $tpaquis;
-
-$std->ideestabadquir = $ideestabadquir;
 
 try {
     //carrega a classe responsavel por lidar com os certificados
@@ -83,7 +57,11 @@ try {
     $certificate = Certificate::readPfx($content, $password);
 
     //cria o evento e retorna o XML assinado
-    $xml = Event::evtAqProd($configJson, $std, $certificate, '2017-08-03 10:37:00' //opcional data e hora
+    $xml = Event::evtBenPrRP(
+        $configJson,
+        $std,
+        $certificate,
+        '2017-08-03 10:37:00' //opcional data e hora
     )->toXml();
 
     header('Content-type: text/xml; charset=UTF-8');
