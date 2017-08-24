2ac485a00e5655e72ef3ab67d715a8cc2c076b4e
robmachado <linux.rlm@gmail.com>
2017-08-24 16:36:58 -0300

    [UPD] ISSUE #72 Evento EvtTabHorTur

--- src/Factories/EvtTabHorTur.php
+++ src/Factories/EvtTabHorTur.php
@@ -65,18 +65,9 @@ class EvtTabHorTur extends Factory implements FactoryInterface
      */
     protected function toNode()
     {
-        $evtid        = FactoryId::build(
-            $this->tpInsc,
-            $this->nrInsc,
-            $this->date,
-            $this->sequencial
-        );
-        $eSocial      = $this->dom->getElementsByTagName("eSocial")->item(0);
-        $evtTabHorTur = $this->dom->createElement("evtTabHorTur");
-        $att          = $this->dom->createAttribute('Id');
-        $att->value   = $evtid;
-        $evtTabHorTur->appendChild($att);
-
+        $ideEmpregador = $this->node->getElementsByTagName('ideEmpregador')->item(0);
+        //o idEvento pode variar de evento para evento
+        //então cada factory individualmente terá de construir o seu
         $ideEvento = $this->dom->createElement("ideEvento");
         $this->dom->addChild(
             $ideEvento,
@@ -96,24 +87,125 @@ class EvtTabHorTur extends Factory implements FactoryInterface
             $this->verProc,
             true
         );
-        $evtTabHorTur->appendChild($ideEvento);
-
-        $ideEmpregador = $this->dom->createElement("ideEmpregador");
+        $this->node->insertBefore($ideEvento, $ideEmpregador);
+        
+        $ide = $this->dom->createElement("ideHorContratual");
         $this->dom->addChild(
-            $ideEmpregador,
-            "tpInsc",
-            $this->tpInsc,
+            $ide,
+            "codHorContrat",
+            $this->std->codhorcontrat,
             true
         );
         $this->dom->addChild(
-            $ideEmpregador,
-            "nrInsc",
-            $this->nrInsc,
+            $ide,
+            "iniValid",
+            $this->std->inivalid,
             true
         );
-        $evtTabHorTur->appendChild($ideEmpregador);
-
-        $eSocial->appendChild($evtTabHorTur);
-        $this->sign($eSocial);
+        $this->dom->addChild(
+            $ide,
+            "fimValid",
+            ! empty($this->std->fimvalid) ? $this->std->fimvalid : null,
+            false
+        );
+        
+        if (!empty($this->std->dadoshorcontratual)) {
+            $da = $this->std->dadoshorcontratual;
+            $dados = $this->dom->createElement("dadosHorContratual");
+            $this->dom->addChild(
+                $dados,
+                "hrEntr",
+                $da->hrentr,
+                true
+            );
+            $this->dom->addChild(
+                $dados,
+                "hrSaida",
+                $da->hrsaida,
+                true
+            );
+            $this->dom->addChild(
+                $dados,
+                "durJornada",
+                $da->durjornada,
+                true
+            );
+            $this->dom->addChild(
+                $dados,
+                "perHorFlexivel",
+                $da->perhorflexivel,
+                true
+            );
+            if (!empty($da->horarioIntervalo)) {
+                foreach ($da->horarioIntervalo as $inter) {
+                    $intervalo = $this->dom->createElement("horarioIntervalo");
+                    $this->dom->addChild(
+                        $intervalo,
+                        "tpInterv",
+                        $inter->tpinterv,
+                        true
+                    );
+                    $this->dom->addChild(
+                        $intervalo,
+                        "durInterv",
+                        $inter->durinterv,
+                        true
+                    );
+                    $this->dom->addChild(
+                        $intervalo,
+                        "iniInterv",
+                        !empty($inter->iniinterv) ? $inter->iniinterv : null,
+                        false
+                    );
+                    $this->dom->addChild(
+                        $intervalo,
+                        "termInterv",
+                        !empty($inter->terminterv) ? $inter->terminterv : null,
+                        false
+                    );
+                    $dados->appendChild($intervalo);
+                }
+            }
+        }
+        
+        if (!empty($this->std->novavalidade)) {
+            $nova = $this->dom->createElement("novaValidade");
+            $this->dom->addChild(
+                $nova,
+                "iniValid",
+                $this->std->novavalidade->inivalid,
+                true
+            );
+            $this->dom->addChild(
+                $nova,
+                "fimValid",
+                ! empty($this->std->novavalidade->fimvalid)
+                    ? $this->std->novavalidade->fimvalid
+                    : null,
+                false
+            );
+        }
+        
+        $info = $this->dom->createElement("infoHorContratual");
+        //seleção do modo
+        if ($this->std->modo == 'INC') {
+            $node = $this->dom->createElement("inclusao");
+            $node->appendChild($ide);
+            $node->appendChild($dados);
+        } elseif ($this->std->modo == 'ALT') {
+            $node = $this->dom->createElement("alteracao");
+            $node->appendChild($ide);
+            $node->appendChild($dados);
+            $node->appendChild($nova);
+        } else {
+            $node = $this->dom->createElement("exclusao");
+            $node->appendChild($ide);
+        }
+        
+        $info->appendChild($node);
+        $this->node->appendChild($info);
+        $this->eSocial->appendChild($this->node);
+        //$this->xml = $this->dom->saveXML($this->eSocial);
+        $this->sign();
     }
 }
