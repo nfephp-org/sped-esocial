874f866bebe06b3d9b0fd527e06d8c819ea0b749
robmachado <linux.rlm@gmail.com>
2017-08-24 15:36:04 -0300

    [UPD] ISSUE #71 Evento EvtTabFuncao

--- src/Factories/EvtTabFuncao.php
+++ src/Factories/EvtTabFuncao.php
@@ -65,18 +65,9 @@ class EvtTabFuncao extends Factory implements FactoryInterface
      */
     protected function toNode()
     {
-        $evtid        = FactoryId::build(
-            $this->tpInsc,
-            $this->nrInsc,
-            $this->date,
-            $this->sequencial
-        );
-        $eSocial      = $this->dom->getElementsByTagName("eSocial")->item(0);
-        $evtTabFuncao = $this->dom->createElement("evtTabFuncao");
-        $att          = $this->dom->createAttribute('Id');
-        $att->value   = $evtid;
-        $evtTabFuncao->appendChild($att);
-
+        $ideEmpregador = $this->node->getElementsByTagName('ideEmpregador')->item(0);
+        //o idEvento pode variar de evento para evento
+        //então cada factory individualmente terá de construir o seu
         $ideEvento = $this->dom->createElement("ideEvento");
         $this->dom->addChild(
             $ideEvento,
@@ -96,24 +87,84 @@ class EvtTabFuncao extends Factory implements FactoryInterface
             $this->verProc,
             true
         );
-        $evtTabFuncao->appendChild($ideEvento);
-
-        $ideEmpregador = $this->dom->createElement("ideEmpregador");
+        $this->node->insertBefore($ideEvento, $ideEmpregador);
+        
+        $ide = $this->dom->createElement("ideFuncao");
         $this->dom->addChild(
-            $ideEmpregador,
-            "tpInsc",
-            $this->tpInsc,
+            $ide,
+            "codFuncao",
+            $this->std->codfuncao,
             true
         );
         $this->dom->addChild(
-            $ideEmpregador,
-            "nrInsc",
-            $this->nrInsc,
+            $ide,
+            "iniValid",
+            $this->std->inivalid,
             true
         );
-        $evtTabFuncao->appendChild($ideEmpregador);
-
-        $eSocial->appendChild($evtTabFuncao);
-        $this->sign($eSocial);
+        $this->dom->addChild(
+            $ide,
+            "fimValid",
+            ! empty($this->std->fimvalid) ? $this->std->fimvalid : null,
+            false
+        );
+        
+        
+        if (!empty($this->std->dadosfuncao)) {
+            $da = $this->std->dadosfuncao;
+            $dados = $this->dom->createElement("dadosFuncao");
+            $this->dom->addChild(
+                $dados,
+                "dscFuncao",
+                $da->dscfuncao,
+                true
+            );
+            $this->dom->addChild(
+                $dados,
+                "codCBO",
+                $da->codcbo,
+                true
+            );
+        }
+        
+        if (!empty($this->std->novavalidade)) {
+            $nova = $this->dom->createElement("novaValidade");
+            $this->dom->addChild(
+                $nova,
+                "iniValid",
+                $this->std->novavalidade->inivalid,
+                true
+            );
+            $this->dom->addChild(
+                $nova,
+                "fimValid",
+                ! empty($this->std->novavalidade->fimvalid)
+                    ? $this->std->novavalidade->fimvalid
+                    : null,
+                false
+            );
+        }
+        
+        $info = $this->dom->createElement("infoFuncao");
+        //seleção do modo
+        if ($this->std->modo == 'INC') {
+            $node = $this->dom->createElement("inclusao");
+            $node->appendChild($ide);
+            $node->appendChild($dados);
+        } elseif ($this->std->modo == 'ALT') {
+            $node = $this->dom->createElement("alteracao");
+            $node->appendChild($ide);
+            $node->appendChild($dados);
+            $node->appendChild($nova);
+        } else {
+            $node = $this->dom->createElement("exclusao");
+            $node->appendChild($ide);
+        }
+        
+        $info->appendChild($node);
+        $this->node->appendChild($info);
+        $this->eSocial->appendChild($this->node);
+        //$this->xml = $this->dom->saveXML($this->eSocial);
+        $this->sign();
     }
 }
