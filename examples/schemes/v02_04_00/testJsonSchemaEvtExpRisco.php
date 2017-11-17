92135b2bb5d98b50564e2751d14fd0a24b90603b
robmachado <linux.rlm@gmail.com>
2017-11-17 09:37:20 -0200

    [UPD] ISSUE #52 EvtExpRisco

--- examples/schemes/v02_04_00/testJsonSchemaEvtExpRisco.php
+++ examples/schemes/v02_04_00/testJsonSchemaEvtExpRisco.php
@@ -261,7 +261,7 @@ $std->nrrecibo = null;
 $std->cpftrab = '12345678901';
 $std->nistrab = '12345678901';
 $std->matricula = '002zcbv';
-$std->respreg[] = new \stdClass();
+$std->respreg[0] = new \stdClass();
 $std->respreg[0]->dtini = '2015-02-04';
 $std->respreg[0]->dtfim = null;
 $std->respreg[0]->nisresp = '12345678901';
@@ -270,12 +270,12 @@ $std->respreg[0]->ufoc = 'SP';
 $std->modo = 'INI'; //['INI', 'ALT', 'FIM']
 $std->dtcondicao = '2016-02-01';
 
-$std->infoamb[] = new \stdClass();
+$std->infoamb[0] = new \stdClass();
 $std->infoamb[0]->codamb = 'abcdefg';
 
 //opcional depende do modo
 $std->infoamb[0]->dscativdes = 'Descricao das atividades, fisicas ou mentais, realizadas pelo trabalhador, por forca do poder de comando a que se submete.';
-$std->infoamb[0]->fatrisco[] = new \stdClass();
+$std->infoamb[0]->fatrisco[0] = new \stdClass();
 $std->infoamb[0]->fatrisco[0]->codfatris = '01.01.012';
 $std->infoamb[0]->fatrisco[0]->intconc = '20 mSv';
 $std->infoamb[0]->fatrisco[0]->tecmedicao = 'dosimetro Geiger- Muller de halogenio';
@@ -283,11 +283,11 @@ $std->infoamb[0]->fatrisco[0]->epcepi = new \stdClass();
 $std->infoamb[0]->fatrisco[0]->epcepi->utilizepc = 1;// 0 - Não se aplica; 1 - Não utilizado; 2 - Utilizado.
 $std->infoamb[0]->fatrisco[0]->epcepi->utilizepi = 1;//0 - Não se aplica; 1 - Não utilizado; 2 - Utilizado
 //opcional
-$std->infoamb[0]->fatrisco[0]->epcepi->epc[] = new \stdClass();
+$std->infoamb[0]->fatrisco[0]->epcepi->epc[0] = new \stdClass();
 $std->infoamb[0]->fatrisco[0]->epcepi->epc[0]->dscepc = 'barreira de contencao';
 $std->infoamb[0]->fatrisco[0]->epcepi->epc[0]->eficepc = 'S'; //S - Sim; N - Não.
 //opcional
-$std->infoamb[0]->fatrisco[0]->epcepi->epi[] = new \stdClass();
+$std->infoamb[0]->fatrisco[0]->epcepi->epi[0] = new \stdClass();
 $std->infoamb[0]->fatrisco[0]->epcepi->epi[0]->caepi = 'macacao';
 $std->infoamb[0]->fatrisco[0]->epcepi->epi[0]->eficepi = 'S';
 $std->infoamb[0]->fatrisco[0]->epcepi->epi[0]->medprotecao = 'S';
