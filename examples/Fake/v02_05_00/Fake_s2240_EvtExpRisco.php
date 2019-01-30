286c1316fcbfec41c897a17184262f43cf6b35e0
robmachado <linux.rlm@gmail.com>
2019-01-30 10:19:19 -0200

    [UPD] Atualização dos XSD de 21/01/19 e ajuste em S2240 EvtExpRisco

--- examples/Fake/v02_05_00/Fake_s2240_EvtExpRisco.php
+++ examples/Fake/v02_05_00/Fake_s2240_EvtExpRisco.php
@@ -47,7 +47,7 @@ $std->infoamb[1]->codamb = 'xxxxx';
 $std->infoativ = new \stdClass();
 $std->infoativ->dscativdes = 'lkskslkslsklsks  lsk slsklsk';
 $std->infoativ->ativpericinsal[0] = new \stdClass();
-$std->infoativ->ativpericinsal[0]->codativ = '221111';
+$std->infoativ->ativpericinsal[0]->codativ = '22.111';
 
 $std->fatrisco[0] = new \stdClass();
 $std->fatrisco[0]->codfatris = '01.01.012';
