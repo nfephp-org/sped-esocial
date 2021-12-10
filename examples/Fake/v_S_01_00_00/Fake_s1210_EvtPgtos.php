af3107033f1a48f1a5c787b6f374d4162ae58b68
robmachado <linux.rlm@gmail.com>
2021-12-10 11:27:11 -0300

    inclusão S-1210

--- examples/Fake/v_S_01_00_00/Fake_s1210_EvtPgtos.php
+++ examples/Fake/v_S_01_00_00/Fake_s1210_EvtPgtos.php
@@ -30,113 +30,20 @@ $configJson = json_encode($config, JSON_PRETTY_PRINT);
 
 
 $std = new \stdClass();
-$std->sequencial = 1;
-$std->indretif = 1;
-$std->nrrecibo = 'abcdefghijklmnopq';
-$std->indapuracao = 2;
-$std->perapur = '2017-12';
-$std->cpfbenef = '12345678901';
+//$std->sequencial = 1; //Opcional
+$std->indretif = 1; //Obrigatório
+$std->nrrecibo = null; //Opcional
+$std->perapur = '2017-12';  //Obrigatório
+$std->indguia = 1; //Opcional
+$std->cpfbenef = '12345678901';  //Obrigatório
 
-$std->deps = new \stdClass();
-$std->deps->vrdeddep = 1000.00;
-
-$std->infopgto[0] = new \stdClass();
-$std->infopgto[0]->dtpgto = '2018-01-15';
-$std->infopgto[0]->tppgto = 4;
-$std->infopgto[0]->indresbr = 'N';
-
-$std->infopgto[0]->detpgtofl[0] = new \stdClass();
-$std->infopgto[0]->detpgtofl[0]->perref = '2018-12';
-$std->infopgto[0]->detpgtofl[0]->idedmdev = 'jlkjkj112121';
-$std->infopgto[0]->detpgtofl[0]->indpgtott = 'N';
-$std->infopgto[0]->detpgtofl[0]->vrliq = 1001.55;
-$std->infopgto[0]->detpgtofl[0]->nrrecarq = 'dkjhdkjdhkjdh dkjhdkjhdkj';
-
-$std->infopgto[0]->detpgtofl[0]->retpgtotot[0] = new \stdClass();
-$std->infopgto[0]->detpgtofl[0]->retpgtotot[0]->codrubr = 'kjlsksksjs';
-$std->infopgto[0]->detpgtofl[0]->retpgtotot[0]->idetabrubr = 'k2k3k2k3';
-$std->infopgto[0]->detpgtofl[0]->retpgtotot[0]->qtdrubr = 1500.22;
-$std->infopgto[0]->detpgtofl[0]->retpgtotot[0]->fatorrubr = 11.55;
-$std->infopgto[0]->detpgtofl[0]->retpgtotot[0]->vrunit = 5894.56;
-$std->infopgto[0]->detpgtofl[0]->retpgtotot[0]->vrrubr = 8759.99;
-
-$std->infopgto[0]->detpgtofl[0]->retpgtotot[0]->penalim[0] = new \stdClass();
-$std->infopgto[0]->detpgtofl[0]->retpgtotot[0]->penalim[0]->cpfbenef = '12345678901';
-$std->infopgto[0]->detpgtofl[0]->retpgtotot[0]->penalim[0]->dtnasctobenef = '2011-04-15';
-$std->infopgto[0]->detpgtofl[0]->retpgtotot[0]->penalim[0]->nmbenefic = 'Beltrano de Tal';
-$std->infopgto[0]->detpgtofl[0]->retpgtotot[0]->penalim[0]->vlrpensao = 1200.87;
-
-$std->infopgto[0]->detpgtofl[0]->infopgtoparc[0] = new \stdClass();
-$std->infopgto[0]->detpgtofl[0]->infopgtoparc[0]->matricula = 'slkjslskjslkjslksjks';
-$std->infopgto[0]->detpgtofl[0]->infopgtoparc[0]->codrubr = 'sdkdjkdjdkjdkjd 29322929';
-$std->infopgto[0]->detpgtofl[0]->infopgtoparc[0]->idetabrubr = 'ksks1234';
-$std->infopgto[0]->detpgtofl[0]->infopgtoparc[0]->qtdrubr = 1000.34;
-$std->infopgto[0]->detpgtofl[0]->infopgtoparc[0]->fatorrubr = 2.54;
-$std->infopgto[0]->detpgtofl[0]->infopgtoparc[0]->vrunit = 1200.55;
-$std->infopgto[0]->detpgtofl[0]->infopgtoparc[0]->vrrubr = 3988.77;
-
-$std->infopgto[0]->detpgtobenpr = new \stdClass();
-$std->infopgto[0]->detpgtobenpr->perref = '2018-02';
-$std->infopgto[0]->detpgtobenpr->idedmdev = 'ljslksjksjksj';
-$std->infopgto[0]->detpgtobenpr->indpgtott = 'N';
-$std->infopgto[0]->detpgtobenpr->vrliq = 234.44;
-
-$std->infopgto[0]->detpgtobenpr->retpgtotot[0] = new \stdClass();
-$std->infopgto[0]->detpgtobenpr->retpgtotot[0]->codrubr = 'sdkdjkdjdkjdkjd 29322929';
-$std->infopgto[0]->detpgtobenpr->retpgtotot[0]->idetabrubr = 'ksks1234';
-$std->infopgto[0]->detpgtobenpr->retpgtotot[0]->qtdrubr = 2012.33;
-$std->infopgto[0]->detpgtobenpr->retpgtotot[0]->fatorrubr = 3.66;
-$std->infopgto[0]->detpgtobenpr->retpgtotot[0]->vrunit = 234.98;
-$std->infopgto[0]->detpgtobenpr->retpgtotot[0]->vrrubr = 2654.87;
-
-$std->infopgto[0]->detpgtobenpr->infopgtoparc[0] = new \stdClass();
-$std->infopgto[0]->detpgtobenpr->infopgtoparc[0]->codrubr = 'sdkdjkdjdkjdkjd 29322929';
-$std->infopgto[0]->detpgtobenpr->infopgtoparc[0]->idetabrubr = 'ksks1234';
-$std->infopgto[0]->detpgtobenpr->infopgtoparc[0]->qtdrubr = 2012.33;
-$std->infopgto[0]->detpgtobenpr->infopgtoparc[0]->fatorrubr = 3.66;
-$std->infopgto[0]->detpgtobenpr->infopgtoparc[0]->vrunit = 234.98;
-$std->infopgto[0]->detpgtobenpr->infopgtoparc[0]->vrrubr = 2654.87;
-
-$std->infopgto[0]->detpgtofer[0] = new \stdClass();
-$std->infopgto[0]->detpgtofer[0]->codcateg = 101;
-$std->infopgto[0]->detpgtofer[0]->matricula = 'slkjslskjslkjslksjks';
-$std->infopgto[0]->detpgtofer[0]->dtinigoz = '2018-03-01';
-$std->infopgto[0]->detpgtofer[0]->qtdias = 22;
-$std->infopgto[0]->detpgtofer[0]->vrliq = 2658.25;
-
-$std->infopgto[0]->detpgtofer[0]->detrubrfer[0] = new \stdClass();
-$std->infopgto[0]->detpgtofer[0]->detrubrfer[0]->codrubr = 'sdkdjkdjdkjdkjd 29322929';
-$std->infopgto[0]->detpgtofer[0]->detrubrfer[0]->idetabrubr = 'ksks1234';
-$std->infopgto[0]->detpgtofer[0]->detrubrfer[0]->qtdrubr = 2012.33;
-$std->infopgto[0]->detpgtofer[0]->detrubrfer[0]->fatorrubr = 3.66;
-$std->infopgto[0]->detpgtofer[0]->detrubrfer[0]->vrunit = 234.98;
-$std->infopgto[0]->detpgtofer[0]->detrubrfer[0]->vrrubr = 2654.87;
-
-$std->infopgto[0]->detpgtofer[0]->detrubrfer[0]->penalim[0] = new \stdClass();
-$std->infopgto[0]->detpgtofer[0]->detrubrfer[0]->penalim[0]->cpfbenef = '12345678901';
-$std->infopgto[0]->detpgtofer[0]->detrubrfer[0]->penalim[0]->dtnasctobenef = '2011-04-15';
-$std->infopgto[0]->detpgtofer[0]->detrubrfer[0]->penalim[0]->nmbenefic = 'Beltrano de Tal';
-$std->infopgto[0]->detpgtofer[0]->detrubrfer[0]->penalim[0]->vlrpensao = 1200.87;
-
-$std->infopgto[0]->detpgtoant[0] = new \stdClass();
-$std->infopgto[0]->detpgtoant[0]->codcateg = 101;
-
-$std->infopgto[0]->detpgtoant[0]->infopgtoant[0] = new \stdClass();
-$std->infopgto[0]->detpgtoant[0]->infopgtoant[0]->tpbcirrf = '01';
-$std->infopgto[0]->detpgtoant[0]->infopgtoant[0]->vrbcirrf = 3452.87;
-
-$std->infopgto[0]->idepgtoext = new \stdClass();
-$std->infopgto[0]->idepgtoext->codpais = '008';
-$std->infopgto[0]->idepgtoext->indnif = 3;
-$std->infopgto[0]->idepgtoext->nifbenef = '298983927937';
-
-$std->infopgto[0]->idepgtoext->endext = new \stdClass();
-$std->infopgto[0]->idepgtoext->endext->dsclograd = 'lkjslkjsksj';
-$std->infopgto[0]->idepgtoext->endext->nrlograd = '123';
-$std->infopgto[0]->idepgtoext->endext->complem = 'kjdhkdjdhj';
-$std->infopgto[0]->idepgtoext->endext->bairro = 'kjdlkdjkdjk';
-$std->infopgto[0]->idepgtoext->endext->nmcid = 'lkwjlkjekj';
-$std->infopgto[0]->idepgtoext->endext->codpostal = '1230099';
+//Informações dos pagamentos efetuados.
+$std->infopgto[0] = new \stdClass(); //Obrigatório
+$std->infopgto[0]->dtpgto = '2018-01-15';  //Obrigatório
+$std->infopgto[0]->tppgto = 4;  //Obrigatório
+$std->infopgto[0]->perref = '2020';  //Obrigatório
+$std->infopgto[0]->idedmdev = 'sjksjskjslsjksjsj';  //Obrigatório
+$std->infopgto[0]->vrliq = 1000.33;  //Obrigatóri
 
 
 try {
