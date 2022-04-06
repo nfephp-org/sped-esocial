637873297b8042e2087812932740ef7816f91005
Anderson <andersonlsilverio@gmail.com>
2022-04-06 16:39:05 -0300

    Destaca campo como opcional no exemplo.

--- examples/schemes/v_S_01_00_00/s2200_JsonSchemaEvtAdmissao.php
+++ examples/schemes/v_S_01_00_00/s2200_JsonSchemaEvtAdmissao.php
@@ -1012,7 +1012,7 @@ $std->vinculo->infocontrato->localtempdom->codmunic = '1234567';
 $std->vinculo->infocontrato->localtempdom->uf = 'AC';
 
 $std->vinculo->infocontrato->horcontratual = new \stdClass();
-$std->vinculo->infocontrato->horcontratual->qtdhrssem = 99.50;
+$std->vinculo->infocontrato->horcontratual->qtdhrssem = 99.50; //Opcional
 $std->vinculo->infocontrato->horcontratual->tpjornada = 9;
 $std->vinculo->infocontrato->horcontratual->dsctpjorn = 'kjsksjsjs';
 $std->vinculo->infocontrato->horcontratual->tmpparc = 0;
