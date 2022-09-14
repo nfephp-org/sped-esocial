21fcdd8bc2319ac2e481eca4fc6a2f0ebade7bcf
Fabio Egidio <fabio.egidio@dbseller.com.br>
2022-09-14 18:37:45 -0300

    Correcao da validacao de pensao por morte

--- src/Factories/Traits/TraitS2410.php
+++ src/Factories/Traits/TraitS2410.php
@@ -137,26 +137,26 @@ trait TraitS2410
             false
         );
         
-        if (!empty($this->std->infobeninicio->infopenmorte)) {
+        if (!empty($this->std->infobeninicio->dadosbeneficio->infopenmorte)) {
             $infoPenMorte = $this->dom->createElement("infoPenMorte");
             $this->dom->addChild(
                 $infoPenMorte,
                 "tpPenMorte",
-                $this->std->infobeninicio->infopenmorte->tppenmorte,
+                $this->std->infobeninicio->dadosbeneficio->infopenmorte->tppenmorte,
                 true
             );
-            if (!empty($this->std->infobeninicio->infopenmorte->instpenmorte)) {
+            if (!empty($this->std->infobeninicio->dadosbeneficio->infopenmorte->instpenmorte)) {
                 $instPenMorte = $this->dom->createElement("instPenMorte");
                 $this->dom->addChild(
                     $instPenMorte,
                     "cpfInst",
-                    $this->std->infobeninicio->infopenmorte->instpenmorte->cpfinst,
+                    $this->std->infobeninicio->dadosbeneficio->infopenmorte->instpenmorte->cpfinst,
                     true
                 );
                 $this->dom->addChild(
                     $instPenMorte,
                     "dtInst",
-                    $this->std->infobeninicio->infopenmorte->instpenmorte->dtinst,
+                    $this->std->infobeninicio->dadosbeneficio->infopenmorte->instpenmorte->dtinst,
                     true
                 );
                 $infoPenMorte->appendChild($instPenMorte);
