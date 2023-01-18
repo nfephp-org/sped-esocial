77f54bd546af87dcf3e1f115ec4982b441a4af5b
lucasjarrier <lucas.cavalcanti@ccc.ufcg.edu.br>
2023-01-18 09:55:50 -0300

    Atualização Schema Esocial S1000

--- src/Factories/Traits/TraitS1000.php
+++ src/Factories/Traits/TraitS1000.php
@@ -707,13 +707,13 @@ trait TraitS1000
             $this->dom->addChild(
                 $infoCadastro,
                 "dtTrans11096",
-                !empty($this->std->cad->dttrans11096) ? $this->std->cad->dttrans11096 : null,
+                !empty($cad->dttrans11096) ? $cad->dttrans11096 : null,
                 false
             );
             $this->dom->addChild(
                 $infoCadastro,
                 "indTribFolhaPisCofins",
-                !empty($this->std->cad->indtribfolhapiscofins) ? $this->std->cad->indtribfolhapiscofins : null,
+                !empty($cad->indtribfolhapiscofins) ? $cad->indtribfolhapiscofins : null,
                 false
             );
         }
