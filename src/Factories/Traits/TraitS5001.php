1e437f0be8920abe876b9e7c153c719eae7f4d8a
andrehanke <QmNsXaLNfUZ645DRcnjN>
2023-09-29 11:32:14 -0300

    trait s5001 adicionado

---
+++ src/Factories/Traits/TraitS5001.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace NFePHP\eSocial\Factories\Traits;
+
+trait TraitS5001
+{
+
+    /**
+     * builder for version 2.5.0
+     */
+    protected function toNode250()
+    {
+        throw new \Exception("NÃO EXISTE EVENTO {$this->evtAlias} na versão 2.5.0 !!");
+    }
+
+    /**
+     * builder for version S.1.0.0
+     */
+    protected function toNodeS100()
+    {
+        throw new \Exception("NÃO EXISTE EVENTO {$this->evtAlias} na versão 1.0.0 !!");
+    }
+
+    /**
+     * builder for version S.1.1.0
+     */
+    protected function toNodeS110()
+    {
+        $ideEmpregador = $this->node->getElementsByTagName('ideEmpregador')->item(0);
+        //o idEvento pode variar de evento para evento
+        //então cada factory individualmente terá de construir o seu
+        $ideEvento = $this->dom->createElement("ideEvento");
+        $this->dom->addChild(
+            $ideEvento,
+            "nrRecArqBase",
+            $this->std->nrRecArqBase,
+            true
+        );
+        $this->dom->addChild(
+            $ideEvento,
+            "indApuracao",
+            $this->std->indApuracao,
+            true
+        );
+        $this->dom->addChild(
+            $ideEvento,
+            "perApur",
+            $this->std->perApur,
+            true
+        );
+        $this->node->insertBefore($ideEvento, $ideEmpregador);
+        $ide = $this->dom->createElement("ideTrabalhador");
+        $this->dom->addChild(
+            $ide,
+            "cpfTrab",
+            $this->std->cpftrab,
+            true
+        );
+    }
+}
