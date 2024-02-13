fb4a739d5b936ddefb949d19422928815d0a9a11
robmachado <linux.rlm@gmail.com>
2024-02-13 15:27:08 -0300

    ajustes evtCdBenefAlt estCiv

--- examples/schemes/v_S_01_02_00/s2405_JsonSchemaEvtCdBenefAlt.php
+++ examples/schemes/v_S_01_02_00/s2405_JsonSchemaEvtCdBenefAlt.php
@@ -66,8 +66,8 @@ $jsonSchema = '{
                     "maximum": 6
                 },
                 "estciv": {
-                    "required": true,
-                    "type": "integer",
+                    "required": false,
+                    "type": ["integer","null"],
                     "minimum": 1,
                     "maximum": 5
                 },
